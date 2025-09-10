1088790b6ab916c4093c8fa386308a8f3b313d03
hieudv <hieudv@hivetech.vn>
2025-09-10 22:12:53 +0700

    bai30 jsx- fragment , import css in component

--- src/component/lean/MyComponent.jsx
+++ src/component/lean/MyComponent.jsx
@@ -1,7 +1,21 @@
 
+import './style.css';
+
+
+// JSX : là sức mạnh của react cho phép code HTML trong file Javascript
+// trong file jsx chỉ có một cha html duy nhất ta có thể dùng thẻ div bọc ngoài nhưng các này dễ làm phá vỡ cấu trúc css nên ta dùng fragment 
+// Fragment (mảnh vỡ) giúp bạn viết code ngắn di và không render "thừa html
+
 const MyComponent = () => {
   return(
-    <div> Hieu and React update</div>
+    <>
+     <div> Hieu and React update</div>
+    <div className="child">
+        Viết trong HTML thêm CSS 
+    </div>
+    
+    </>
+   
   )
 }
 
