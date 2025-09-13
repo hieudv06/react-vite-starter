f2917eaee1fc97f1b4fe7e478bfd1a2b2e5faf9f
hieudv <hieudv@hivetech.vn>
2025-09-13 21:47:30 +0700

    bai 36 DOM Events

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,12 +2,27 @@ const TodoNew = (props) =>{
 
   const {addNewTodo} = props
 
+  // addNewToDo()  // fire
+  const handleClick =  () =>{
+    alert("click me")
+  }
+
+  const handleOnChange = (name) =>{
+    console.log(">>> handleOnChange",name)
+  }
+
   // addNewTodo("hieudv");
 
     return(
         <div className='todo-new'>
-      <input type="text"/>
-      <button>Add</button>
+      <input type="text"
+        onChange={(event) =>handleOnChange(event.target.value)}
+
+      />
+      <button
+      style={{cusor: "pointer"}}
+      onClick={handleClick}
+      >Add</button>
     </div>
     )
 }
